aef57663432da58c8b07bc1c65b8cc5c8e7cc02b
Tyson Gern <tgern@sovrn.com>
2025-09-25 09:58:42 -0600

    Run for a duration

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func DivideUint64(numerator, denominator uint64) float64 {
 // EstimatePi estimates the value of π using Monte Carlo simulation.
 // It returns the estimated value of π and the number of iterations performed.
 // It stops when the keepGoing parameter returns false.
-func EstimatePi(points iter.Seq[Point], keepGoing func(uint64) bool) (pi float64, iterations uint64) {
+func EstimatePi(points iter.Seq[Point], keepGoing func() bool) (pi float64, iterations uint64) {
 	var totalPoints, insidePoints uint64
 
 	for point := range points {
@@ -56,7 +56,7 @@ func EstimatePi(points iter.Seq[Point], keepGoing func(uint64) bool) (pi float64
 		if point.InsideUnitCircle() {
 			insidePoints++
 		}
-		if !keepGoing(totalPoints) {
+		if !keepGoing() {
 			break
 		}
 	}
@@ -74,28 +74,40 @@ func ExecutionTime(description string) func() {
 	}
 }
 
+func RunFor(delay time.Duration) func() bool {
+	sigtermChannel := make(chan os.Signal, 1)
+	signal.Notify(sigtermChannel, os.Interrupt, syscall.SIGTERM)
+	timer := time.NewTimer(delay)
+	cancel := make(chan interface{})
+	go func() {
+		select {
+		case <-sigtermChannel:
+		case <-timer.C:
+		}
+		close(sigtermChannel)
+		timer.Stop()
+		close(cancel)
+	}()
+	
+	return func() bool {
+		select {
+		case <-cancel:
+			return false
+		default:
+			return true
+		}
+	}
+}
+
 func main() {
-	var maxIterations uint64
-	flag.Uint64Var(&maxIterations, "n", 1_000_000_000, "maximum number of iterations")
+	var duration int
+	flag.IntVar(&duration, "n", 20, "execution duration in seconds")
 	flag.Parse()
 
 	defer ExecutionTime("Estimation")()
-	sigtermChannel := make(chan os.Signal, 1)
-	signal.Notify(sigtermChannel, os.Interrupt, syscall.SIGTERM)
 
 	points := GeneratePoints()
-	pi, iterations := EstimatePi(points, func(iteration uint64) bool {
-		if iteration%10_000_000 == 0 {
-			print(".")
-		}
+	pi, iterations := EstimatePi(points, RunFor(time.Duration(duration) * time.Second))
 
-		select {
-		case <-sigtermChannel:
-			return false
-		default:
-			return iteration < maxIterations
-		}
-	})
-
-	fmt.Printf("\nπ ≈ %.12f (n=%d)\n", pi, iterations)
+	fmt.Printf("π ≈ %.12f (n=%d)\n", pi, iterations)
 }
