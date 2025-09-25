cd90eefdffb0bade5b2f4c24ca3c76abe9352336
Tyson Gern <tgern@sovrn.com>
2025-09-25 11:42:14 -0600

    Parallelize calculation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"iter"
@@ -8,34 +9,37 @@ import (
 	"math/rand/v2"
 	"os"
 	"os/signal"
+	"runtime"
+	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
 )
 
-// Point represents a point in 2D space.
 type Point struct {
 	X, Y float64
 }
 
-// InsideUnitCircle checks if a point is inside the unit circle.
 func (p Point) InsideUnitCircle() bool {
 	return p.X*p.X+p.Y*p.Y <= 1
 }
 
-// GeneratePoints generates random points within the unit square.
-// It returns an iterable sequence of Point structs.
-func GeneratePoints() iter.Seq[Point] {
+func GeneratePoints(ctx context.Context) iter.Seq[Point] {
 	return func(yield func(Point) bool) {
 		for {
 			ok := yield(Point{X: rand.Float64(), Y: rand.Float64()})
 			if !ok {
 				return
 			}
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
 		}
 	}
 }
 
-// DivideUint64 divides two uint64 numbers and returns the result as a float64.
 func DivideUint64(numerator, denominator uint64) float64 {
 	bigNumerator := new(big.Int).SetUint64(numerator)
 	bigDenominator := new(big.Int).SetUint64(denominator)
@@ -45,28 +49,33 @@ func DivideUint64(numerator, denominator uint64) float64 {
 	return approx
 }
 
-// EstimatePi estimates the value of π using Monte Carlo simulation.
-// It returns the estimated value of π and the number of iterations performed.
-// It stops when the keepGoing parameter returns false.
-func EstimatePi(points iter.Seq[Point], keepGoing func() bool) (pi float64, iterations uint64) {
-	var totalPoints, insidePoints uint64
-
+func CountInsideUnitCircle(ctx context.Context, points iter.Seq[Point]) (total, inside uint64) {
 	for point := range points {
-		totalPoints++
+		total++
 		if point.InsideUnitCircle() {
-			insidePoints++
-		}
-		if !keepGoing() {
-			break
+			inside++
 		}
 	}
-
-	return DivideUint64(insidePoints, totalPoints) * 4, totalPoints
+	return total, inside
 }
 
-// ExecutionTime measures time between when it is called and when the function it returns is called
-// and prints the result.
-func ExecutionTime(description string) func() {
+func EstimatePi(ctx context.Context, numberOfWorkers int) (pi float64, iterations uint64) {
+	var totalPoints, insidePoints atomic.Uint64
+
+	wg := sync.WaitGroup{}
+	for _ = range numberOfWorkers {
+		wg.Go(func() {
+			total, inside := CountInsideUnitCircle(ctx, GeneratePoints(ctx))
+			totalPoints.Add(total)
+			insidePoints.Add(inside)
+		})
+	}
+	wg.Wait()
+
+	return DivideUint64(insidePoints.Load(), totalPoints.Load()) * 4, totalPoints.Load()
+}
+
+func ExecutionTimer(description string) func() {
 	start := time.Now()
 	return func() {
 		elapsed := time.Since(start)
@@ -74,40 +83,33 @@ func ExecutionTime(description string) func() {
 	}
 }
 
-func RunFor(delay time.Duration) func() bool {
+func SigtermTimeoutContext(timeout time.Duration) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	sigtermChannel := make(chan os.Signal, 1)
 	signal.Notify(sigtermChannel, os.Interrupt, syscall.SIGTERM)
-	timer := time.NewTimer(delay)
-	cancel := make(chan interface{})
 	go func() {
-		select {
-		case <-sigtermChannel:
-		case <-timer.C:
-		}
-		close(sigtermChannel)
-		timer.Stop()
-		close(cancel)
+		<-sigtermChannel
+		cancel()
 	}()
-	
-	return func() bool {
-		select {
-		case <-cancel:
-			return false
-		default:
-			return true
-		}
-	}
+	return ctx, cancel
+}
+
+func ParseCommandLineArgs() (duration, numberOfWorkers int) {
+	flag.IntVar(&duration, "d", 20, "execution duration in seconds")
+	flag.IntVar(&numberOfWorkers, "n", runtime.NumCPU(), "number of workers")
+	flag.Parse()
+	fmt.Printf("Running for %d seconds with %d workers\n", duration, numberOfWorkers)
+	return duration, numberOfWorkers
 }
 
 func main() {
-	var duration int
-	flag.IntVar(&duration, "n", 20, "execution duration in seconds")
-	flag.Parse()
+	duration, numberOfWorkers := ParseCommandLineArgs()
 
-	defer ExecutionTime("Estimation")()
+	ctx, cancel := SigtermTimeoutContext(time.Duration(duration) * time.Second)
+	defer cancel()
+	done := ExecutionTimer("Estimation")
+	defer done()
 
-	points := GeneratePoints()
-	pi, iterations := EstimatePi(points, RunFor(time.Duration(duration) * time.Second))
-
+	pi, iterations := EstimatePi(ctx, numberOfWorkers)
 	fmt.Printf("π ≈ %.12f (n=%d)\n", pi, iterations)
 }
