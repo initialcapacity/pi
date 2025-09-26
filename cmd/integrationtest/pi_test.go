d88aee47b54b59be22c356ee763782083f747353
Tyson Gern <tgern@sovrn.com>
2025-09-26 12:18:12 -0400

    Add integration test

---
+++ cmd/integrationtest/pi_test.go
@@ -0,0 +1,43 @@
+package integration_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/tygern/pi/internal/assert"
+)
+
+func TestIntegration(t *testing.T) {
+	prepareBuildDirectory(t)
+	build(t, "pi")
+	
+	testCtx, cancelCtx := context.WithCancel(t.Context())
+	defer cancelCtx()
+
+	output := runCommand(t, testCtx, "./build/pi", "-d", "1", "-n", "2")
+
+	assert.ContainsSubstring(t, output, "Running for 1")
+	assert.ContainsSubstring(t, output, "with 2 workers")
+	assert.ContainsSubstring(t, output, "π ≈ 3.14")
+}
+
+func prepareBuildDirectory(t *testing.T) {
+	err := os.RemoveAll("../../build")
+	assert.NoError(t, err)
+	err = os.MkdirAll("./build", os.ModePerm)
+	assert.NoError(t, err)
+}
+
+func build(t *testing.T, name string) {
+	err := exec.Command("go", "build", "-o", fmt.Sprintf("./build/%s", name), fmt.Sprintf("../../cmd/%s", name)).Run()
+	assert.NoError(t, err)
+}
+
+func runCommand(t *testing.T, ctx context.Context, command string, arguments ...string) string {
+	output, err := exec.CommandContext(ctx, command, arguments...).Output()
+	assert.NoError(t, err)
+	return string(output)
+}
--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -2,6 +2,7 @@ package assert
 
 import (
 	"cmp"
+	"strings"
 	"testing"
 )
 
@@ -9,7 +10,7 @@ func Equal[V comparable](t testing.TB, expected, actual V) {
 	t.Helper()
 
 	if expected != actual {
-		t.Errorf(`Test assertion failed.
+		t.Fatalf(`Test assertion failed.
 expected: %v
 actual: %v`, expected, actual)
 	}
@@ -19,7 +20,25 @@ func GreaterThanOrEqualTo[V cmp.Ordered](t testing.TB, first, second V) {
 	t.Helper()
 
 	if first < second {
-		t.Errorf(`Test assertion failed.
+		t.Fatalf(`Test assertion failed.
 expected %v to be greater than or equal to %v`, first, second)
+	}
+}
+
+func NoError(t testing.TB, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Fatalf(`Test assertion failed.
+expected error '%v' to be nil`, err)
+	}
+}
+
+func ContainsSubstring(t testing.TB, subject, substring string) {
+	t.Helper()
+
+	if !strings.Contains(subject, substring) {
+		t.Fatalf(`Test assertion failed.
+expected '%v' to contain '%v'`, subject, substring)
 	}
 }
